ddb9caf68ab11c95ab26eeea93779408d4e2483d
Pierre CROKAERT <pct@actl.be>
2014-11-01 19:24:26 +0100

    Rellback to Architect 2

--- app/DirectAPI.js
+++
@@ -1,9 +0,0 @@
-Ext.define('inficare.DirectAPI', {
-    requires: ['Ext.direct.*']
-}, function() {
-    var Loader = Ext.Loader,
-        wasLoading = Loader.isLoading;
-    Loader.loadScriptFile('php/api.php', Ext.emptyFn, Ext.emptyFn, null, true);
-    Loader.isLoading = wasLoading;
-    Ext.direct.Manager.addProvider(Ext.app.REMOTING_API);
-});
